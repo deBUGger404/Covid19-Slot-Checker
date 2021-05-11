b8e1ef11edfe9f7deddaf4c412bdf8e628904839
Rakesh Kumar <59862546+chiku-1436@users.noreply.github.com>
2021-05-11 12:58:23 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
-mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+# mkdir -p ~/.streamlit/
+# echo "\
+# [server]\n\
+# headless = true\n\
+# port = $PORT\n\
+# enableCORS = false\n\
+# \n\
+# " > ~/.streamlit/config.toml
 
+mkdir -p ~/.streamlit/
+echo "[general]"  > ~/.streamlit/credentials.toml
+echo "email = \"rakeshparmuwal@gmail.com\""  >> ~/.streamlit/credentials.toml
+echo "[server]"  > ~/.streamlit/config.toml 
+echo "headless = true"  >> ~/.streamlit/config.toml
+echo "port = $PORT"  >> ~/.streamlit/config.toml
+echo "enableCORS = true"  >> ~/.streamlit/config.toml
