6493a289700c7647cf715882a1c94964957159ba
Rakesh Kumar <59862546+chiku-1436@users.noreply.github.com>
2021-05-11 13:01:23 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,9 @@
 # " > ~/.streamlit/config.toml
 
 mkdir -p ~/.streamlit/
-echo "[general]"  > ~/.streamlit/credentials.toml
-echo "email = \"rakeshparmuwal@gmail.com\""  >> ~/.streamlit/credentials.toml
-echo "[server]"  > ~/.streamlit/config.toml 
-echo "headless = true"  >> ~/.streamlit/config.toml
-echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = true"  >> ~/.streamlit/config.toml
+echo "[general]  
+email = \"rakeshparmuwal1436@gmail.com\""  > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false"  >> ~/.streamlit/config.toml
